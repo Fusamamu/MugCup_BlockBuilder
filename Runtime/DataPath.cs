fc3b3c5b032d0bda8437b0b077b3ebe0ce00904c
Sukum Duangpattra <tonypengfusama@gmail.com>
2022-01-25 20:44:17 +0700

    Scriptable Setting Name Changed

--- Runtime/DataPath.cs
+++ Runtime/DataPath.cs
@@ -6,9 +6,9 @@ namespace MugCup_BlockBuilder.Runtime.Core
 {
     public static class DataPath
     {
-        public const string GridDataSettingPath      = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/GridDataSetting.asset";
-        public const string DefaultMeshBlockDataPath = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/BlockMeshData.asset";
-        public const string AssetCollectionPath      = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/AssetCollection.asset";
-        public const string MaterialDataPath         = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/MaterialDataSetting.asset";
+        public const string GridDataSettingPath      = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/DefaultGridDataSetting.asset";
+        public const string DefaultMeshBlockDataPath = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/DefaultBlockMeshData.asset";
+        public const string AssetCollectionPath      = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/DefaultAssetCollection.asset";
+        public const string MaterialDataPath         = "Packages/com.mugcupp.mugcup-blockbuilder/Editor Resources/Setting/DefaultMaterialDataSetting.asset";
     }
 }
